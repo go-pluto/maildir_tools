67e06682b87fdc7d44c001f3735049172fa345a0
Lennart Oldenburg <hello@lennartoldenburg.de>
2017-07-06 21:02:55 +0200

    Adjusted walk-related functions to use new internal Maildir representation and provide real Prometheus metrics.

--- walk.go
+++ walk.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"hash"
 	"os"
 
 	"crypto/sha512"
@@ -8,98 +10,19 @@ import (
 	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
 )
-
-// walkUserMaildir takes the Maildir root path for
-// one user of the exported system as input and
-// deterministically builds a file system representation
-// of MaildirItems for all folders and files below
-// this path. It also calculates initial metrics.
-func (m *UserMaildir) walkUserMaildir(userPath string) error {
-
-	shaHash := sha512.New()
-	m.Items = make([]MaildirItem, 0, 10)
-
-	err := filepath.Walk(userPath, func(path string, info os.FileInfo, err error) error {
-
-		if err != nil {
-			return err
-		}
-
-		// Do not include the user's Maildir root path,
-		// but add it to this user's file system watcher.
-		if path == userPath {
-
-			absPath, err := filepath.Abs(path)
-			if err != nil {
-				return err
-			}
-
-			err = m.Watcher.Add(absPath)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-		// Maildirs only consist of folders and files,
-		// thus ignore all other elements.
-		if !(info.IsDir() || info.Mode().IsRegular()) {
-			return nil
-		}
-
-		size := uint64(info.Size())
-
-		// Add this file system object to the items list.
-		m.Items = append(m.Items, MaildirItem{
-			Path: path,
-			Size: size,
-		})
-
-		if info.IsDir() {
-
-			m.Metrics["maildir_folders_total"]++
-
-			absPath, err := filepath.Abs(path)
-			if err != nil {
-				return err
-			}
-
-			// Add this sub directory to this user's watcher.
-			err = m.Watcher.Add(absPath)
-			if err != nil {
-				return err
-			}
-		} else if info.Mode().IsRegular() {
-			m.Metrics["maildir_files_total"]++
-		}
-
-		m.Metrics["maildir_elements_total"]++
-		m.Metrics["maildir_size_total"] += size
-
-		shaHash.Write([]byte(path))
-
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
-	m.Checksum = shaHash.Sum(nil)
-
-	return nil
-}
 
 // walkRootMaildir expects the path to the monitored
 // node's Maildir directory containing one folder per
-// user in the system. It then walks each user folder
-// individually and deterministically by invoking the
-// above function walkUserMaildir.
-func walkRootMaildir(maildirRootPath string) (*[]UserMaildir, error) {
+// user in the system. It creates initial structures
+// for per user file system walks.
+func walkRootMaildir(maildirRootPath string) ([]*UserMaildir, error) {
 
 	i := 0
-	userMaildirs := make([]UserMaildir, 0, 30)
+	userMaildirs := make([]*UserMaildir, 0, 30)
 
 	// Retrieve all file system elements in maildirRootPath.
 	files, _ := ioutil.ReadDir(maildirRootPath)
@@ -114,30 +37,122 @@ func walkRootMaildir(maildirRootPath string) (*[]UserMaildir, error) {
 			}
 
 			// Create new item for this user.
-			userMaildirs = append(userMaildirs, UserMaildir{
-				done:    make(chan struct{}),
-				Metrics: make(map[string]uint64),
-				Items:   nil,
-				Watcher: w,
+			userMaildirs = append(userMaildirs, &UserMaildir{
+				userPath:     filepath.Join(maildirRootPath, f.Name()),
+				watcher:      w,
+				walkTrigger:  make(chan struct{}),
+				watchTrigger: make(chan struct{}),
+				done:         make(chan struct{}),
 			})
-
-			// Set metrics initially to zero.
-			userMaildirs[i].Metrics["maildir_elements_total"] = 0
-			userMaildirs[i].Metrics["maildir_folders_total"] = 0
-			userMaildirs[i].Metrics["maildir_files_total"] = 0
-			userMaildirs[i].Metrics["maildir_size_total"] = 0
-
-			maildirPath := filepath.Join(maildirRootPath, f.Name())
-
-			// Inspect user folder content individually.
-			err = userMaildirs[i].walkUserMaildir(maildirPath)
-			if err != nil {
-				return nil, err
-			}
 
 			i++
 		}
 	}
 
-	return &userMaildirs, nil
+	return userMaildirs, nil
+}
+
+// walk deterministically builds the file system
+// representation of MaildirItems for all folders
+// and files below the user's Maildir path when
+// triggered. It also calculates initial metrics.
+func (m *UserMaildir) walk(logger log.Logger, metrics *Metrics, done chan struct{}) {
+
+	// Close channel on function exit.
+	defer close(m.walkTrigger)
+
+	for {
+
+		select {
+
+		case <-m.walkTrigger:
+
+			var numElems float64 = 0.0
+			var numFolders float64 = 0.0
+			var numFiles float64 = 0.0
+			var numSize float64 = 0.0
+			var shaHash hash.Hash = sha512.New()
+
+			err := filepath.Walk(m.userPath, func(path string, info os.FileInfo, err error) error {
+
+				if err != nil {
+					return err
+				}
+
+				// Do not include the user's Maildir root path,
+				// but add it to this user's file system watcher.
+				if path == m.userPath {
+
+					absPath, err := filepath.Abs(path)
+					if err != nil {
+						return err
+					}
+
+					err = m.watcher.Add(absPath)
+					if err != nil {
+						return err
+					}
+
+					return nil
+				}
+
+				// Maildirs only consist of folders and files,
+				// thus ignore all other elements.
+				if !(info.IsDir() || info.Mode().IsRegular()) {
+					return nil
+				}
+
+				if info.IsDir() {
+
+					numFolders++
+
+					absPath, err := filepath.Abs(path)
+					if err != nil {
+						return err
+					}
+
+					// Add this sub directory to this user's watcher.
+					err = m.watcher.Add(absPath)
+					if err != nil {
+						return err
+					}
+				} else if info.Mode().IsRegular() {
+					numFiles++
+				}
+
+				numElems++
+				numSize += float64(info.Size())
+
+				// Add element to checksum calculation.
+				shaHash.Write([]byte(path))
+
+				return nil
+			})
+			if err != nil {
+				level.Error(logger).Log(
+					"msg", "error while walking user Maildir",
+					"err", err,
+				)
+				return
+			}
+
+			// Set updated metrics in supplied struct.
+			metrics.elements.With(prometheus.Labels{"user": m.userPath}).Set(numElems)
+			metrics.folders.With(prometheus.Labels{"user": m.userPath}).Set(numFolders)
+			metrics.files.With(prometheus.Labels{"user": m.userPath}).Set(numFiles)
+
+			// Include the calculated SHA512 checksum for this Maildir.
+			metrics.size.With(prometheus.Labels{
+				"user":   m.userPath,
+				"sha512": fmt.Sprintf("%x", shaHash.Sum(nil)),
+			}).Set(numSize)
+
+			// Signal walk completion downstream.
+			done <- struct{}{}
+
+		case <-m.done:
+			level.Debug(logger).Log("msg", fmt.Sprintf("done walking Maildir for %s", m.userPath))
+			return
+		}
+	}
 }
